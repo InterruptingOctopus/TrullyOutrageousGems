1695528df80c42b450484ba48b585e67e0f07c06
InterruptingOctopus <cesar.quesada1996@gmail.com>
2016-11-10 03:49:10 -0500

    another attempt to fix

--- src/main/java/interruptingoctopus/trulyoutrageousgems/blocks/BlockGarnetOre.java
+++ src/main/java/interruptingoctopus/trulyoutrageousgems/blocks/BlockGarnetOre.java
@@ -3,6 +3,8 @@ package interruptingoctopus.trulyoutrageousgems.blocks;
 import interruptingoctopus.trulyoutrageousgems.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
 
 public class BlockGarnetOre extends Block {
 
@@ -12,5 +14,19 @@ public class BlockGarnetOre extends Block {
 		setRegistryName(Reference.TogemsBlocks.GARNETORE.getRegistryName());
 		setHardness(5.0F);
 		}
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+	
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+	
+	@Override
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
 
 }
